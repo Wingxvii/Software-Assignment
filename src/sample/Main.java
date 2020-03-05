ab312e013f68631833cfb785c52ace1d575b7edf
John <john.wang@uoit.net>
2020-03-05 00:29:03 -0500

    cleanup and comments

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -25,12 +25,10 @@ import javafx.stage.FileChooser;
 
 public class Main extends Application {
 
-
     Stage _primaryStage;
     Scene startScene;
 
     //problem 3 data
-    double radius = 10;
     Circle bigCircle = new Circle(120,120,100, Color.WHITE);
     Circle smallCircle1 = new Circle(120,20,10,Color.RED);
     Circle smallCircle2 = new Circle(20,120,10,Color.RED);
@@ -46,88 +44,99 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        //setup
         _primaryStage = primaryStage;
-
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Assignment 1");
 
+        //create main menu buttons
         GridPane startPane = new GridPane();
 
+        //button for q1
         Button q1button = new Button("Question 1");
-
         q1button.setOnAction(value ->  {
             System.out.println("Problem 1");
             _primaryStage.setScene(Question1Setup(root));
             _primaryStage.show();
         });
 
+        //button for q2
         Button q2button = new Button("Question 2");
-
         q2button.setOnAction(value ->  {
             System.out.println("Problem 2");
             _primaryStage.setScene(Question2Setup(root));
             _primaryStage.show();
         });
 
+        //button for q3
         Button q3button = new Button("Question 3");
-
         q3button.setOnAction(value ->  {
                 System.out.println("Problem 3");
                 _primaryStage.setScene(Question3Setup(root));
                 _primaryStage.show();
         });
 
+        //button for q4
         Button q4button = new Button("Question 4");
-
         q4button.setOnAction(value ->  {
             System.out.println("Problem 4");
             _primaryStage.setScene(Question4Setup(root));
             _primaryStage.show();
         });
 
+        //add buttons
         startPane.add(q1button,0,0);
         startPane.add(q2button,1,0);
         startPane.add(q3button,2,0);
         startPane.add(q4button,3,0);
 
+        //setup stage
         startScene = new Scene(startPane);
         primaryStage.setScene(startScene);
         primaryStage.show();
     }
 
+
+    //code for question 1
     public Scene Question1Setup(Parent root){
         GridPane problemPane = new GridPane();
 
+        //3 random cards
         for(int counter = 0; counter < 3; counter++) {
             int rand = (int)(Math.random() * 53);
             ImageView newImage = new ImageView("./images/" + rand + ".png");
             problemPane.add(newImage, counter, 1);
         }
 
+        //setup return button
         Button returnButton = new Button("Return");
-
         returnButton.setOnAction(value ->  {
                 _primaryStage.setScene(startScene);
                 _primaryStage.show();
         });
         problemPane.add(returnButton,0,0);
 
+        //return
         Scene problemScene = new Scene(problemPane);
         return problemScene;
     }
+    //code for question 2
     public Scene Question2Setup(Parent root){
         GridPane problemPane = new GridPane();
 
+        //init labels
         Label investmentLabel = new Label("Investment Amount: ");
         Label yearsLabel = new Label("Years: ");
         Label annualIntrestLabel= new Label("Annual Intrest Rate: ");
         Label futureLabel = new Label("Future Value: ");
 
+        //init text fields
         TextField investmentField = new TextField();
         TextField yearsField = new TextField();
         TextField annualIntrestField = new TextField();
         Label futureField = new Label("");
 
+        //calculate
         Button calculate = new Button ("Calculate");
         calculate.setOnAction(value ->  {
             double result = 0.0;
@@ -135,8 +144,7 @@ public class Main extends Application {
             futureField.setText(Double.toString(result));
         });
 
-        Button returnButton = new Button("Return");
-
+        //add nodes to pane
         problemPane.add(investmentLabel,0,1);
         problemPane.add(yearsLabel,0,2);
         problemPane.add(annualIntrestLabel,0,3);
@@ -149,33 +157,43 @@ public class Main extends Application {
 
         problemPane.add(calculate,1,5);
 
+        //setup return button
+        Button returnButton = new Button("Return");
         returnButton.setOnAction(value ->  {
             _primaryStage.setScene(startScene);
             _primaryStage.show();
         });
         problemPane.add(returnButton,0,0);
 
+        //return
         Scene problemScene = new Scene(problemPane);
         return problemScene;
     }
+    //code for question 3
     public Scene Question3Setup(Parent root){
         GridPane outterProblemPane = new GridPane();
         Pane problemPane = new Pane();
 
+        //setup shapes
         bigCircle.setStroke(Color.BLACK);
         updateAngle();
         updateLines();
         problemPane.getChildren().addAll(bigCircle, smallCircle1, smallCircle2, smallCircle3, line1, line2, line3, text1, text2, text3);
 
+        //setup mouse drag events
         smallCircle1.setOnMouseDragged(e -> {
+            //calculate point on circle
             double vX = e.getX() - 120;
             double vY = e.getY() - 120;
             double magV = Math.sqrt(vX*vX + vY*vY);
             double aX = 120 + vX / magV * 100;
             double aY = 120 + vY / magV * 100;
 
+            //move small circle
             smallCircle1.setCenterX(aX);
             smallCircle1.setCenterY(aY);
+
+            //update
             updateAngle();
             updateLines();
         });
@@ -206,44 +224,53 @@ public class Main extends Application {
             updateLines();
         });
 
+        //add to pane
+        outterProblemPane.add(problemPane, 0,1);
+
+        //setup buttons
         Button returnButton = new Button("Return");
         returnButton.setOnAction(value ->  {
             _primaryStage.setScene(startScene);
             _primaryStage.show();
         });
-
         outterProblemPane.add(returnButton,0,0);
-        outterProblemPane.add(problemPane, 0,1);
 
+        //return
         Scene problemScene = new Scene(outterProblemPane, 300, 250);
         return problemScene;
     }
+    //code for question 4
     public Scene Question4Setup(Parent root ){
         GridPane problemPane = new GridPane();
         FileChooser fileChooser = new FileChooser();
 
+        //setup graph
         final CategoryAxis xAxis = new CategoryAxis();
         final NumberAxis yAxis = new NumberAxis();
         final BarChart<String,Number> bc = new BarChart<String,Number>(xAxis,yAxis);
         bc.setTitle("All Letters");
         xAxis.setLabel("Letters");
         yAxis.setLabel("Value");
-        //problem 4 data
 
+        //init chart
         int[] chars = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
         updateGraph(chars, bc);
 
+        //setup file selection
         Button fileChooserButton = new Button("Select Text File");
         FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("Text Files", "*.txt");
         fileChooser.getExtensionFilters().add(extFilter);
 
+        //file selection
         fileChooserButton.setOnAction(e -> {
             File selectedFile = fileChooser.showOpenDialog(_primaryStage);
+            //read from file
             try {
                 BufferedReader br = new BufferedReader(new FileReader(selectedFile.toString()));
                 String st;
                 int[] allchars = new int[26];
 
+                //parse through all lines
                 while ((st = br.readLine()) != null){
 
                     char[] dataChar = st.toCharArray();
@@ -258,26 +285,31 @@ public class Main extends Application {
                     }
                 }
 
+                //call update
                 updateGraph(allchars, bc);
 
-            }catch(FileNotFoundException ex){
+            }
+            //error handling
+            catch(FileNotFoundException ex){
                 System.out.println("Error");
             }catch (IOException ex){
                 System.out.println("Error");
             }
         });
 
+        //add to pane
         problemPane.add(fileChooserButton,0,2);
         problemPane.add(bc,0,1);
 
+        //setup button
         Button returnButton = new Button("Return");
-
         returnButton.setOnAction(value ->  {
             _primaryStage.setScene(startScene);
             _primaryStage.show();
         });
         problemPane.add(returnButton,0,0);
 
+        //return
         Scene problemScene = new Scene(problemPane);
         return problemScene;
     }
@@ -290,15 +322,15 @@ public class Main extends Application {
         double c = new Point2D(smallCircle2.getCenterX(), smallCircle2.getCenterY()).distance(smallCircle1.getCenterX(), smallCircle1.getCenterY());
 
         text1.setX(smallCircle1.getCenterX());
-        text1.setY(smallCircle1.getCenterY() - radius);
+        text1.setY(smallCircle1.getCenterY() - 10);
         text1.setText(String.format("%.2f", Math.toDegrees(Math.acos((a * a - b * b - c * c) / (-2 * b * c)))));
 
         text2.setX(smallCircle2.getCenterX());
-        text2.setY(smallCircle2.getCenterY() - radius);
+        text2.setY(smallCircle2.getCenterY() - 10);
         text2.setText(String.format("%.2f", Math.toDegrees(Math.acos((b * b - a * a - c * c) / (-2 * a * c)))));
 
         text3.setX(smallCircle3.getCenterX());
-        text3.setY(smallCircle3.getCenterY() - radius);
+        text3.setY(smallCircle3.getCenterY() - 10);
         text3.setText(String.format("%.2f", Math.toDegrees(Math.acos((c * c - b * b - a * a) / (-2 * a * b)))));
 
     }
